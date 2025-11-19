39628423b6e53b43a6549c1237f5b1b570449e68
Aref Rahnama <arefrhnm1@gmail.com>
2025-11-18 18:28:49 -0800

    feat(api): integrate json-server mock API and use it across pages with dynamic params (basic UI)

---
+++ src/app/store/[id]/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Container from "../../../components/Container";
+import { IProductItemProps } from "../../../components/ProductItem";
+
+interface IProductProps {
+	params: Promise<{ id: string }>;
+	searchParams: Promise<{}>;
+}
+
+export default async function Product({ params }: IProductProps) {
+	const { id } = await params;
+
+	const result = await fetch(`http://localhost:3001/products/${id}`);
+	const data = (await result.json()) as IProductItemProps;
+	return (
+		<Container>
+			<div className="grid grid-cols-12 gap-10">
+				<div className="col-span-3">
+					<img src={data.image} />
+				</div>
+				<div className="col-span-9">
+					<h2>{data.title}</h2>
+					<h4>{data.description}</h4>
+					<span>{data.price}$</span>
+				</div>
+			</div>
+		</Container>
+	);
+}
--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,44 +1,20 @@
 import React from "react";
 import Container from "../../components/Container";
-import ProductItem from "../../components/ProductItem";
+import ProductItem, { IProductItemProps } from "../../components/ProductItem";
+import Link from "next/link";
 
-export default function Store() {
-	const data = [
-		{
-			id: "1",
-			image: "https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg",
-			title: "Aftab parast",
-			description: "a animal with big eye and eat hashare",
-			price: 23,
-		},
-		{
-			id: "2",
-			image: "https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg",
-			title: "Aftab parast2",
-			description: "a animal with big eye and eat hashare",
-			price: 89,
-		},
-		{
-			id: "3",
-			image: "https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg",
-			title: "Aftab parast3",
-			description: "a animal with big eye and eat hashare",
-			price: 13,
-		},
-		{
-			id: "4",
-			image: "https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg",
-			title: "Aftab parast4",
-			description: "a animal with big eye and eat hashare",
-			price: 84,
-		},
-	];
+export default async function Store() {
+
+	const result = await fetch("http://localhost:3001/products")
+	const data = await result.json() as IProductItemProps[]
 
 	return (
 		<Container>
 			<div className="grid grid-cols-4 gap-4 py-4">
 				{data.map((i) => (
-					<ProductItem key={i.id} {...i}/>
+					<Link key={i.id} href={`/store/${i.id}`}>
+						<ProductItem {...i} />
+					</Link>
 				))}
 			</div>
 		</Container>
