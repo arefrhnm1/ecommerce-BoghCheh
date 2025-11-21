f8cea9a12f5d98785b91590f2c9fce42793c4bd6
Aref Rahnama <arefrhnm1@gmail.com>
2025-11-20 16:46:25 -0800

    feat(cart): convert cart from static data to fully dynamic state

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,23 +1,34 @@
+"use client";
+
 import React from "react";
 import Container from "../../components/Container";
 import CartItem from "../../components/CartItem";
+import { useShoppingCartContext } from "../../context/ShoppingCartContext";
 
 export default function cart() {
+	const { cartItems } = useShoppingCartContext();
+
 	return (
 		<Container>
 			<h1 className="py-10">Cart</h1>
 			<div className="">
-				<CartItem />
-				<CartItem />
-				<CartItem />
+				{cartItems.map((item) => (
+					<CartItem key={item.id} {...item} />
+				))}
 			</div>
 			<div className="border shadow-md p-4 grid gap-3">
 				<h3>total price: 384</h3>
 				<h3>Takhfif: 10%</h3>
 				<h3>final price: 350</h3>
 				<div>
-					<input className="border px-4 py-2 mr-2" placeholder="code Takhfif" type="text" />
-					<button className="bg-slate-200 border rounded-lg px-4 py-2">Takhfif</button>
+					<input
+						className="border px-4 py-2 mr-2"
+						placeholder="code Takhfif"
+						type="text"
+					/>
+					<button className="bg-slate-200 border rounded-lg px-4 py-2">
+						Takhfif
+					</button>
 				</div>
 			</div>
 		</Container>
--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { IProductItemProps } from "./ProductItem";
+import AddToCart from "./AddToCart";
 
-function CartItem() {
+interface ICartItemProps {
+	id: number;
+	qty: number;
+}
+
+function CartItem({ id, qty }: ICartItemProps) {
+	const [data, setData] = useState({} as IProductItemProps);
+
+	useEffect(() => {
+		axios(`http://localhost:3001/products/${id}`).then((results) => {
+			const { data } = results;
+
+			setData(data);
+		});
+	}, []);
+
 	return (
 		<div className="grid grid-cols-12 bg-slate-100 mb-4">
 			<img
 				className="col-span-3"
-				src="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg"
+				src={data.image}
 			/>
 			<div className="col-span-9 p-10">
-				<h2>name</h2>
-				<p>count 3</p>
-				<p>price: 39$</p>
+				<h2>{data.title}</h2>
+				<p>{data.description}</p>
+				<p>{qty}</p>
+				<p>price: {data.price}$</p>
 			</div>
+			<AddToCart id={id.toString()}/>
 		</div>
 	);
 }
